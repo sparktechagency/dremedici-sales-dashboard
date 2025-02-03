f77fb222df6e27d65427d70c10ae1c71ea4342f4
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-02-03 10:14:53 +0600

    bar chart update

--- src/Pages/Dashboard/Home.jsx
+++ src/Pages/Dashboard/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { FaDollarSign, FaRegCalendarAlt, FaUsers } from "react-icons/fa";
-import { FaUserGroup } from "react-icons/fa6";
-import { GiHomeGarage } from "react-icons/gi";
+import { FaCalendarDay, FaDollarSign, FaRegCalendarAlt, } from "react-icons/fa";
+import { HiMiniUsers } from "react-icons/hi2";
+import { MdOutlineHome, MdOutlineWarehouse } from "react-icons/md";
 import { IoIosArrowDown } from "react-icons/io";
-import { MdCarRental } from "react-icons/md";
+import { PiHouseLine } from "react-icons/pi";
 import { Bar } from "react-chartjs-2";
 import {
   Chart as ChartJS,
@@ -26,37 +26,38 @@ ChartJS.register(
 const Home = () => {
 const data = {
   labels: [
-    "January",
-    "February",
-    "March",
-    "April",
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
     "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
   ],
   datasets: [
     {
       label: "Subscriptions",
-      data: [64, 70, 55, 83, 60, 65, 80, 70, 67, 74, 65, 70],
+      data: [64, 27, 83, 90, 87, 85, 70, 40, 32, 74, 65, 70],
       backgroundColor: "#3FC7EE",
       borderColor: "#A1A1A1",
       borderWidth: 1,
-      barThickness: 32,
-      maxBarThickness: 32,
+      barThickness: 24,
+      maxBarThickness: 24,
     },
   ],
 };
 
 const options = {
   responsive: true,
+  maintainAspectRatio: false,
   plugins: {
     legend: {
-      display: true,
+      display: false,
     },
   },
   scales: {
@@ -64,7 +65,7 @@ const options = {
       barPercentage: 1.0,
       categoryPercentage: 1.0,
       grid: {
-        display: false, //
+        display: false,
         color: "#A1A1A1",
       },
     },
@@ -76,18 +77,20 @@ const options = {
         suggestedMax: 100,
       },
       grid: {
-        display: true, // 👈 Y-Axis এর গ্রিড লাইন বন্ধ থাকবে
+        display: true,
+        lineWidth: 2,
       },
     },
   },
 };
+
 
   return (
     <div>
       <div className="flex justify-end">
         <div className="flex items-center justify-center bg-white p-2 mb-6 w-80 gap-4 rounded-md">
           <FaRegCalendarAlt className="" />
-          <p className="">01 Feb 2025 - 28 April 2025</p>
+          <p className="">01 Feb 2025 - 28 Apr 2025</p>
           <IoIosArrowDown />
         </div>
       </div>
@@ -96,7 +99,7 @@ const options = {
         <div className="bg-white rounded-lg py-0 px-6 flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <div className=" w-12 h-12 rounded-full bg-[#EFEFEF] flex items-center justify-center ">
-              <GiHomeGarage color="#007BA5" className="" size={24} />
+              <MdOutlineHome color="#007BA5" className="" size={24} />
             </div>
             <div>
               <h3 className=" text-primary text-[32px] font-semibold">100</h3>
@@ -110,7 +113,7 @@ const options = {
         <div className="bg-white rounded-lg py-0 px-6 flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <div className=" w-12 h-12 rounded-full bg-[#EFEFEF] flex items-center justify-center ">
-              <MdCarRental color="#007BA5" className="" size={24} />
+              <PiHouseLine color="#007BA5" className="" size={24} />
             </div>
             <div>
               <h3 className=" text-primary text-[32px] font-semibold">12k</h3>
@@ -124,7 +127,7 @@ const options = {
         <div className="bg-white rounded-lg py-0 px-6 flex items-center justify-between gap-4">
           <div className="flex items-center gap-3">
             <div className=" w-12 h-12 rounded-full bg-[#EFEFEF] flex items-center justify-center ">
-              <FaUsers color="#007BA5" className="" size={24} />
+              <HiMiniUsers color="#007BA5" className="" size={24} />
             </div>
             <div>
               <h3 className=" text-primary text-[32px] font-semibold">500</h3>
@@ -153,29 +156,29 @@ const options = {
       {/* subcription part  */}
       <div className="bg-white p-4 rounded-lg">
         <div className="flex items-center justify-between mb-12">
-          <p>Subscription</p>
-          <div className="flex items-center ">
+          <h2 className="font-semibold">Subscription</h2>
+          <div className="flex items-center py-2 px-3 gap-2 border-2 bg-[#FFFFFF] rounded-lg">
+            <FaCalendarDay />
             <p>2025</p>
             <select>
-              <option value="january">January</option>
-              <option value="february">February</option>
-              <option value="march">March</option>
-              <option value="april">April</option>
+              <option value="january">Jan</option>
+              <option value="february">Feb</option>
+              <option value="march">Mar</option>
+              <option value="april">Apr</option>
               <option value="may">May</option>
-              <option value="june">June</option>
-              <option value="july">July</option>
-              <option value="august">August</option>
-              <option value="september">September</option>
-              <option value="october">October</option>
-              <option value="november">November</option>
-              <option value="december">December</option>
+              <option value="june">Jun</option>
+              <option value="july">Jul</option>
+              <option value="august">Aug</option>
+              <option value="september">Sep</option>
+              <option value="october">Oct</option>
+              <option value="november">Nov</option>
+              <option value="december">Dec</option>
             </select>
           </div>
         </div>
 
-       
         {/* Bar Chart Section */}
-        <div className="mt-6">
+        <div className="mt-6" style={{ height: "200px" }}>
           <Bar data={data} options={options} />
         </div>
       </div>
