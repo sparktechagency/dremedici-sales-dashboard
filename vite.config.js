210bee1eee0dbbfc5679c02f2d01cc11fda921ab
MdJowelAhmed <mdjowelahmed924@gmail.com>
2025-07-14 15:00:15 +0600

    update vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-
+ 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "10.10.7.104",
-    port: 3001,
+    host: "31.97.128.5",
+    // port: 4173,
+  },
+  build: {
+    sourcemap: false,        
+    minify: "esbuild",       
+    chunkSizeWarningLimit: 600, 
   },
 });
